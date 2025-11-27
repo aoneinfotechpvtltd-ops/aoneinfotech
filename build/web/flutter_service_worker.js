8a61ce6764cb8fd94a7a9d4c579750c779de9407
aoneinfotechpvtltd-ops <aoneinfotechpvtltd@gmail.com>
2025-11-28 01:42:03 +0530

    Add build folder for Vercel deploy

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "6e9f863d0492f0f28bab13c40e44f427
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "f7c6e0d79e7d90a75c154de058be1ba6",
+"flutter_bootstrap.js": "13639db4f05dd7e4ceeb6ccaa6b1c46e",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
